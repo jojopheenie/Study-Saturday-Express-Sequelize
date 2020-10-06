715fc923984c05ca7c96d4abe7a2369c80bfe793
Josephine Amos <josephinexamos@gmail.com>
2020-10-05 22:46:17 -0400

    save changes

--- routes/students.js
+++ routes/students.js
@@ -1,4 +1,66 @@
 const router = require('express').Router();
+const Student = require('../db/models/student')
+
+router.get('/', async (req, res, next) => {
+  try {
+    const students = await Student.findAll();
+    res.send(students)
+  } catch(error) {
+      next(error)
+    }
+})
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const id = req.params.id
+    const student = await Student.findById(id)
+    if (!student) {
+      res.status(404).end()
+    } else {
+      res.send(student)
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.post('/', async (req, res, next) => {
+  try {
+    const student = await Student.create(req.body)
+    res.status(201).send(student)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.put('/:id', async (req, res, next) => {
+  try {
+    const [numberOfAffectedRows, updatedStudent] = await Student.update(req.body, {
+      where: {
+        id: req.params.id
+    },
+      returning: true,
+      plain: true
+    })
+    res.send(updatedStudent)
+  } catch (error) {
+    next(error)
+  }
+ })
+
+ router.delete('/:id', async (req, res, next) => {
+   try {
+     await Student.destroy({
+       where: {
+         id: req.params.id
+       }
+     })
+     res.status(204).send('Not Found')
+   } catch (error) {
+     next(error)
+   }
+ })
+
 
 
 module.exports = router;
--- routes/tests.js
+++ routes/tests.js
@@ -1,4 +1,46 @@
+const Test = require('../db/models/test');
 const router = require('express').Router();
 
+router.get('/', async (req, res, next) => {
+  try {
+    const tests = await Test.findAll()
+    res.send(tests)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const test = await Test.findById(req.params.id)
+    res.send(test)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.post('/student/:studentId', async (req, res, next) => {
+  try {
+    let studentTest = req.body
+    studentTest.studentId = req.params.studentId
+    const createdTest = await Test.create(studentTest)
+    res.status(201).send(createdTest)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.delete('/:id', async (req, res, next) => {
+  try {
+    await Test.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    res.status(204).end()
+  } catch (error) {
+    next(error)
+  }
+})
 
 module.exports = router;
